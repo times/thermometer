02a23c59033a9e9915386b4d3a9745b2e8cdad71
Henrique Alves <henriquea@gmail.com>
2013-12-03 17:11:20 +0000

    Added comments view

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from comments.models import Comment
+from api.views import sentiment
 
 def home(request):
-  context = {}
-  return HttpResponse('')
+  comments = []
+  objects = list(Comment.objects.all())
+  for obj in objects:
+    user = {'name': obj.user.name, 'avatar': obj.user.avatar}
+    comments.append({'pub_date': str(obj.pub_date), 'user': user, 'body': obj.comment, 'sentiment': sentiment(obj.comment)})
+  context = { 'comment_list': comments }
+  return render(request, 'comments/index.html', context)
